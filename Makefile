22ac9e4dcddf8614145c05fdda6d9350fb30d2dc
tabletenniser@gmail.com <tabletenniser@gmail.com>
2015-11-16 23:44:49 -0500

    Finish 3.1 global lock

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 
 CC = g++-4.9
 CFLAGS = -O3
+CFLAGS_PTHREAD = -O3 -lpthread
 
-all: randtrack 
+all: randtrack_global_lock
 
 randtrack: list.h hash.h defs.h randtrack.cc
 	$(CC) $(CFLAGS) randtrack.cc -o randtrack
+randtrack_global_lock: list.h hash.h defs.h randtrack_global_lock.cc
+	$(CC) $(CFLAGS_PTHREAD) randtrack_global_lock.cc -o randtrack_global_lock
 
 
 clean:
--- randtrack_global_lock.cc
+++ randtrack_global_lock.cc
@@ -1,34 +1,30 @@
-
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "defs.h"
 #include "hash.h"
 
+// Global constants
 #define SAMPLES_TO_COLLECT   10000000
 #define RAND_NUM_UPPER_BOUND   100000
 #define NUM_SEED_STREAMS            4
 
-/*
- * ECE454 Students:
- * Please fill in the following team struct
- */
 team_t team = {
     "LOL",                  /* Team name */
-
     "Zexuan Wang",                    /* First member full name */
     "998851773",                 /* First member student number */
     "zexuan.wang@mail.utoronto.ca",                 /* First member email address */
-
     "Dropped the Course",                           /* Second member full name */
     "N/A",                           /* Second member student number */
     "N/A"                            /* Second member email address */
 };
 
+// Global variables
+class sample;
+hash<sample,unsigned> h;    // Key is 'unsigned' and element is 'class sample'
 unsigned num_threads;
 unsigned samples_to_skip;
-
-class sample;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 class sample {
     unsigned my_key;
@@ -41,18 +37,52 @@ class sample {
     void print(FILE *f){printf("%d %d\n",my_key,count);}
 };
 
-// This instantiates an empty hash table
-// it is a C++ template, which means we define the types for
-// the element and key value here: element is "class sample" and
-// key value is "unsigned".
-hash<sample,unsigned> h;
+// simple struct that holds the parameters passed into count_frequencies() function.
+struct parameters {
+    int start;
+    int end;
+};
 
-int
-main (int argc, char* argv[]){
+void* count_frequencies(void *args){
     int i,j,k;
     int rnum;
     unsigned key;
     sample *s;
+    parameters *p = (parameters*) args;
+
+    // process streams starting with different initial numbers
+    for (i=p->start; i<p->end; i++){
+        rnum = i;
+        // collect a number of samples
+        for (j=0; j<SAMPLES_TO_COLLECT; j++){
+            // skip a number of samples
+            for (k=0; k<samples_to_skip; k++){
+                // rand_r takes in a random seed value, in this case, memory address of stream value.
+                rnum = rand_r((unsigned int*)&rnum);
+            }
+            // force the sample to be within the range of 0..RAND_NUM_UPPER_BOUND-1
+            key = rnum % RAND_NUM_UPPER_BOUND;
+
+            // ++++++ critial section starts +++++++
+            pthread_mutex_lock(&mutex);
+            // if this sample has not been counted before
+            if (!(s = h.lookup(key))){
+                // insert a new element for it into the hash table
+                s = new sample(key);
+                h.insert(s);
+            }
+            // increment the count for the sample
+            s->count++;
+            pthread_mutex_unlock(&mutex);
+            // ++++++ critial section ends +++++++
+        }
+    }
+
+}
+
+
+// Main Function
+int main (int argc, char* argv[]){
 
     // Print out team information
     printf( "Team Name: %s\n", team.team );
@@ -77,33 +107,18 @@ main (int argc, char* argv[]){
     // initialize a 16K-entry (2**14) hash of empty lists
     h.setup(14);
 
-    // process streams starting with different initial numbers
-    for (i=0; i<NUM_SEED_STREAMS; i++){
-        rnum = i;
-
-        // collect a number of samples
-        for (j=0; j<SAMPLES_TO_COLLECT; j++){
-
-            // skip a number of samples
-            for (k=0; k<samples_to_skip; k++){
-                // rand_r takes in a random seed value, in this case, memory address of stream value.
-                rnum = rand_r((unsigned int*)&rnum);
-            }
-
-            // force the sample to be within the range of 0..RAND_NUM_UPPER_BOUND-1
-            key = rnum % RAND_NUM_UPPER_BOUND;
-
-            // if this sample has not been counted before
-            if (!(s = h.lookup(key))){
-
-                // insert a new element for it into the hash table
-                s = new sample(key);
-                h.insert(s);
-            }
-
-            // increment the count for the sample
-            s->count++;
-        }
+    // Create the threads based on the value of num_threads
+    int i, start = 0, step_size = 4/num_threads;
+    pthread_t threads[num_threads];
+    parameters *p = new parameters[num_threads];    // array of parameters where each thread only uses one element.
+    for (i=0; i< num_threads; i++){
+        p[i].start = start;
+        p[i].end = start+step_size;
+        pthread_create(&threads[i], NULL, count_frequencies, (void*) &p[i]);
+        start+=step_size;
+    }
+    for (i=0; i<num_threads; i++){
+        pthread_join(threads[i],NULL);
     }
 
     // print a list of the frequency of all samples
