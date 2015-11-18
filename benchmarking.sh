f8156bb239cec2d22960b494ce2a24ce427bc130
tabletenniser@gmail.com <tabletenniser@gmail.com>
2015-11-18 02:28:11 -0500

    Update benchmarking script

--- benchmarking.sh
+++ benchmarking.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-N=10
-rm ./.times
-for i in `seq 1 $N`;
+N=5
+# THREAD_NUM=4
+
+for file in ./randtrack ./randtrack_global_lock ./randtrack_tm ./randtrack_list_lock ./randtrack_element_lock ./randtrack_reduction
 do
-    /usr/bin/time -a -p -o ./.times ./randtrack_reduction 4 50 > /dev/null
-    echo $i
+    for thread_num in 1 2 4
+    do
+        echo "FILE: $file THREAD_NUM: $thread_num"
+        rm ./.times
+        for i in `seq 1 $N`;
+        do
+            /usr/bin/time -a -p -o ./.times ${file} ${thread_num} 50 > /dev/null
+            #echo $i
+        done
+        echo "Average user :"
+        cat ./.times | grep user | sed 's/user //' | awk "{s+=\$1} END {print s/$N}"
+        echo "Average real :"
+        cat ./.times | grep real | sed 's/real //' | awk "{s+=\$1} END {print s/$N}"
+    done
 done
-echo "Average user :"
-cat ./.times | grep user | sed 's/user //' | awk "{s+=\$1} END {print s/$N}"
-echo "Average real :"
-cat ./.times | grep real | sed 's/real //' | awk "{s+=\$1} END {print s/$N}"
